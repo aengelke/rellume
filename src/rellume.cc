1ef1b04c8fc92bb3a814a4683f0aaf2e34b55d63
Alexis Engelke <alexis.engelke@googlemail.com>
2019-06-30 20:29:10 +0200

    Add implementation of C API

---
+++ src/rellume.cc
@@ -0,0 +1,70 @@
+/**
+ * This file is part of Rellume.
+ *
+ * (c) 2016-2019, Alexis Engelke <alexis.engelke@googlemail.com>
+ *
+ * Rellume is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License (LGPL)
+ * as published by the Free Software Foundation, either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * Rellume is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Rellume.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * \file
+ **/
+
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <llvm-c/Core.h>
+
+#include <rellume/rellume.h>
+
+#include <llbasicblock-internal.h>
+#include <llfunction-internal.h>
+
+
+void ll_basic_block_add_branches(LLBasicBlock* bb, LLBasicBlock* a, LLBasicBlock* b) {
+    reinterpret_cast<rellume::BasicBlock*>(bb)->AddBranches(reinterpret_cast<rellume::BasicBlock*>(a), reinterpret_cast<rellume::BasicBlock*>(b));
+}
+void ll_basic_block_add_inst(LLBasicBlock* bb, LLInstr* instr) {
+    reinterpret_cast<rellume::BasicBlock*>(bb)->AddInst(instr);
+}
+
+LLFunc* ll_func(LLVMModuleRef mod) {
+    return reinterpret_cast<LLFunc*>(new rellume::Function(llvm::unwrap(mod)));
+}
+void ll_func_enable_overflow_intrinsics(LLFunc* fn, bool enable) {
+    reinterpret_cast<rellume::Function*>(fn)->EnableOverflowIntrinsics(enable);
+}
+void ll_func_enable_fast_math(LLFunc* fn, bool enable) {
+    reinterpret_cast<rellume::Function*>(fn)->EnableFastMath(enable);
+}
+void ll_func_set_stack_size(LLFunc* fn, size_t size) {
+    reinterpret_cast<rellume::Function*>(fn)->SetStackSize(size);
+}
+void ll_func_set_global_base(LLFunc* fn, uintptr_t base, LLVMValueRef value) {
+    reinterpret_cast<rellume::Function*>(fn)->SetGlobalBase(base, llvm::unwrap(value));
+}
+
+LLBasicBlock* ll_func_add_block(LLFunc* fn) {
+    return reinterpret_cast<LLBasicBlock*>(reinterpret_cast<rellume::Function*>(fn)->AddBlock());
+}
+LLVMValueRef ll_func_lift(LLFunc* fn) {
+    return llvm::wrap(reinterpret_cast<rellume::Function*>(fn)->Lift());
+}
+void ll_func_dispose(LLFunc* fn) {
+    delete reinterpret_cast<rellume::Function*>(fn);
+}
+
+int ll_func_decode(LLFunc* func, uintptr_t addr) {
+    return reinterpret_cast<rellume::Function*>(func)->Decode(addr);
+}
