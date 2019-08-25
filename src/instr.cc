24ed9c242a7440341facc6acc99fc62758ed6c4b
Alexis Engelke <alexis.engelke@googlemail.com>
2019-08-25 16:27:12 +0200

    instr: Add CPUID instruction for custom handling

--- src/instr.cc
+++ src/instr.cc
@@ -177,6 +177,7 @@ end_ops:
     case FDI_CALL_IND: llinst.type = LL_INS_CALL; break;
     case FDI_RET: llinst.type = LL_INS_RET; break;
     case FDI_SYSCALL: llinst.type = LL_INS_SYSCALL; break;
+    case FDI_CPUID: llinst.type = LL_INS_CPUID; break;
     case FDI_PUSH: llinst.type = LL_INS_PUSH; break;
     case FDI_PUSHF: llinst.type = LL_INS_PUSHFQ; break;
     case FDI_POP: llinst.type = LL_INS_POP; break;
