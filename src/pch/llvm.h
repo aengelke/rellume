3d6b90bf2fd46106f14cf1c12bfdfda02c035e46
Alexis Engelke <engelke@in.tum.de>
2022-01-14 12:55:57 +0100

    meson: Remove PCH due to lack of benefit

--- src/pch/llvm.h
+++
@@ -1,7 +0,0 @@
-#include <llvm/IR/Constants.h>
-#include <llvm/IR/DerivedTypes.h>
-#include <llvm/IR/Function.h>
-#include <llvm/IR/GlobalValue.h>
-#include <llvm/IR/Instructions.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Type.h>
